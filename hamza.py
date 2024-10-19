043f9fd6babde507098fbe34bad8b9c00a4f6bca
munir9458 <iamumair1709@gmail.com>
2024-10-19 14:16:19 +0300

    Add files via upload

---
+++ hamza.py
@@ -0,0 +1,4 @@
+b=input("plz enter your number")
+c=input("plz enter your name")
+d=input("plz enter your colour")
+print("plz enter your number",b,"plz enter your name",c,"plz enter your colour",d)
